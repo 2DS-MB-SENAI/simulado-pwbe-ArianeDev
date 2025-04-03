0d3c9af5adc004e41b3ae44e1b1d7741ca57544b
Ariane Silva <silvaarianeoliveira2@gmail.com>
2025-04-03 11:23:42 -0300

    Task 2

--- livros/urls.py
+++ livros/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('livros/', views.get_livros_template, name="livros"),
-    #path('api/livros/cadastrar', views.get_livros, name="Cadastrar livros")
+    path('api/livros/', views.get_livros, name="Livros"),
+    path('api/livros/cadastrar', views.get_livros, name="Cadastrar livros")
 ]
--- livros/views.py
+++ livros/views.py
@@ -1,23 +1,27 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import LivroSerializer
+from rest_framework import status
 from .models import Livro
 from .forms import Forms
 
-# @api_view(['GET', 'POST'])
-# def get_livros(request):
-#     if request.method == 'GET':
-#         livros = Livro.objects.all()
-#         serializer = LivroSerializer(livros, many = True)
+@api_view(['GET', 'POST'])
+def get_livros(request):
+    if request.method == 'GET':
+        livros = Livro.objects.all()
+        serializer = LivroSerializer(livros, many = True)
 
-#         return Response(serializer.data), render(request, 'livros.html', {'livros': livros})
+        return Response(serializer.data), render(request, 'livros.html', {'livros': livros})
     
-#     if request.method == 'POST':
-#         serializer = LivroSerializer(data=request.data)
+    if request.method == 'POST':
+        serializer = LivroSerializer(data=request.data)
 
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.errors, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.errors, status=status.HTTP_201_CREATED)
         
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def get_livros_template(request):
