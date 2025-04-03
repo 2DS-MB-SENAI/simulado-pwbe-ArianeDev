043c0b6d073e48fb8326c998add4a7b09ad10237
Ariane Silva <silvaarianeoliveira2@gmail.com>
2025-04-03 09:59:38 -0300

    Task 2

--- livros/urls.py
+++ livros/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('livros/', views.get_livros, name="Livros")
+    path('api/livros/', views.get_livros, name="Livros"),
+    path('api/livros/cadastrar', views.get_livros, name="Cadastrar livros")
 ]
--- livros/views.py
+++ livros/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import LivroSerializer
+from rest_framework import status
 from .models import Livro
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def get_livros(request):
-    livros = Livro.objects.all()
-    serializer = LivroSerializer(livros, many = True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        livros = Livro.objects.all()
+        serializer = LivroSerializer(livros, many = True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = LivroSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.errors, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
